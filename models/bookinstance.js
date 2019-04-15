d92fa0bd009284f5d3fa4bb0383c8bcc032e7c51
ding <ding@qq.com>
2019-04-15 12:08:43 +0800

    设置gitignore

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-
+const moment = require('moment')
 const Schema = mongoose.Schema
 
 const BookInstanceSchema = new Schema({
@@ -19,6 +19,9 @@ const BookInstanceSchema = new Schema({
 // 虚拟属性'url'：藏书副本 URL
 BookInstanceSchema.virtual('url').get(function() {
   return '/catalog/bookinstance/' + this._id
+})
+BookInstanceSchema.virtual('due_back_formatted').get(function() {
+  return moment(this.due_back).format('MMMM Do, YYYY')
 })
 
 // 导出 BookInstancec 模型
