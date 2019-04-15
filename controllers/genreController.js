8934df0a62e92a024d8bd698b4b63a9a9a1f926d
ding <ding@qq.com>
2019-04-15 19:38:05 +0800

    基本功能完成

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -48,60 +48,60 @@ exports.genre_detail = function(req, res, next) {
 }
 
 // 由 GET 显示创建藏书种类的表单
-exports.genre_create_get = (req, res) => {
+exports.genre_create_get = (req, res, next) => {
   res.render('genre_form', { title: 'Create Genre' })
 }
 
 // 由 POST 处理藏书种类创建操作
-exports.genre_create_post = (req, res) => {
+exports.genre_create_post = [
   // Validate that the name field is not empty.
   body('name', 'Genre name required')
     .isLength({ min: 1 })
     .trim(),
-    // Sanitize (trim and escape) the name field.
-    sanitizeBody('name')
-      .trim()
-      .escape(),
-    // Process request after validation and sanitization.
-    (req, res, next) => {
-      // Extract the validation errors from a request.
-      const errors = validationResult(req)
+  // Sanitize (trim and escape) the name field.
+  sanitizeBody('name')
+    .trim()
+    .escape(),
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req)
 
-      // Create a genre object with escaped and trimmed data.
-      var genre = new Genre({ name: req.body.name })
+    // Create a genre object with escaped and trimmed data.
+    var genre = new Genre({ name: req.body.name })
 
-      if (!errors.isEmpty()) {
-        // There are errors. Render the form again with sanitized values/error messages.
-        res.render('genre_form', {
-          title: 'Create Genre',
-          genre: genre,
-          errors: errors.array()
-        })
-        return
-      } else {
-        // Data from form is valid.
-        // Check if Genre with same name already exists.
-        Genre.findOne({ name: req.body.name }).exec(function(err, found_genre) {
-          if (err) {
-            return next(err)
-          }
+    if (!errors.isEmpty()) {
+      // There are errors. Render the form again with sanitized values/error messages.
+      res.render('genre_form', {
+        title: 'Create Genre',
+        genre: genre,
+        errors: errors.array()
+      })
+      return
+    } else {
+      // Data from form is valid.
+      // Check if Genre with same name already exists.
+      Genre.findOne({ name: req.body.name }).exec(function(err, found_genre) {
+        if (err) {
+          return next(err)
+        }
 
-          if (found_genre) {
-            // Genre exists, redirect to its detail page.
-            res.redirect(found_genre.url)
-          } else {
-            genre.save(function(err) {
-              if (err) {
-                return next(err)
-              }
-              // Genre saved. Redirect to genre detail page.
-              res.redirect(genre.url)
-            })
-          }
-        })
-      }
+        if (found_genre) {
+          // Genre exists, redirect to its detail page.
+          res.redirect(found_genre.url)
+        } else {
+          genre.save(function(err) {
+            if (err) {
+              return next(err)
+            }
+            // Genre saved. Redirect to genre detail page.
+            res.redirect(genre.url)
+          })
+        }
+      })
     }
-}
+  }
+]
 
 // 由 GET 显示删除藏书种类的表单
 exports.genre_delete_get = (req, res) => {
--- models/author.js
+++ models/author.js
@@ -1,36 +1,30 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-const Schema = mongoose.Schema;
+const Schema = mongoose.Schema
 
-const AuthorSchema = new Schema(
-  {
-    first_name: {type: String, required: true, max: 100},
-    family_name: {type: String, required: true, max: 100},
-    date_of_birth: {type: Date},
-    date_of_death: {type: Date},
-  }
-);
+const AuthorSchema = new Schema({
+  first_name: { type: String, required: true, max: 100 },
+  family_name: { type: String, required: true, max: 100 },
+  date_of_birth: { type: Date },
+  date_of_death: { type: Date }
+})
 
 // 虚拟属性'name'：表示作者全名
-AuthorSchema
-  .virtual('name')
-  .get(function () {
-    return this.family_name + ', ' + this.first_name;
-  });
+AuthorSchema.virtual('name').get(function() {
+  return this.family_name + ', ' + this.first_name
+})
 
 // 虚拟属性'lifespan'：作者寿命
-AuthorSchema
-  .virtual('lifespan')
-  .get(function () {
-    return (this.date_of_death.getYear() - this.date_of_birth.getYear()).toString();
-  });
+AuthorSchema.virtual('lifespan').get(function() {
+  return (
+    this.date_of_death.getYear() - this.date_of_birth.getYear()
+  ).toString()
+})
 
 // 虚拟属性'url'：作者 URL
-AuthorSchema
-  .virtual('url')
-  .get(function () {
-    return '/catalog/author/' + this._id;
-  });
+AuthorSchema.virtual('url').get(function() {
+  return '/catalog/author/' + this._id
+})
 
 // 导出 Author 模型
-module.exports = mongoose.model('Author', AuthorSchema);
+module.exports = mongoose.model('Author', AuthorSchema)
